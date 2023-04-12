b0e03f9afe463f245d5e853da2279de7ff7d79bb
OlegKoc <koralll6@yandex.ru>
2023-04-12 21:49:09 +0400

    Added classes

--- src/main/java/com/olegkochurov/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/olegkochurov/spring/mvc_hibernate_aop/controller/MyController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,20 +19,30 @@ public class MyController {
 
     @RequestMapping("/")    // метод, который возвращает всех работников
     public String showAllEmployees(Model model) {
-        List<Employee> employees =  employeeService.getAllEmployees(); // сохраняем в employees
+        List<Employee> employees = employeeService.getAllEmployees(); // сохраняем в employees
         model.addAttribute("allEmps", employees); // теперь в атрибуте allEmps работники которые мы получили из БД
         return "all-emplloyees";  // название view
     }
-@RequestMapping("/addNewEmployee")
-    public String addNewEmployee(Model model){
+
+    @RequestMapping("/addNewEmployee") // метод добавляет работников
+    public String addNewEmployee(Model model) {
         Employee employee = new Employee();
         model.addAttribute("employee", employee);  // атрибут "employee" связывается с JSP страницой
         return "employee-info";
 
     }
-@RequestMapping("/saveEmployee")
-    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+
+    @RequestMapping("/saveEmployee")  // метод сохраняет работников
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         employeeService.saveEmployee(employee);
         return "redirect:/";
+    }
+
+    @RequestMapping("/updateInfo")  // метод изменяет работника
+    public String updateEmployee(@RequestParam("empId") int id, Model model) {
+        Employee employee = employeeService.getEmployee(id);   //
+        model.addAttribute("employee", employee);
+        return "employee-info";
+
     }
 }
--- src/main/webapp/WEB-INF/view/all-emplloyees.jsp
+++ src/main/webapp/WEB-INF/view/all-emplloyees.jsp
@@ -1,5 +1,5 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
 
 <!doctype html>
@@ -10,18 +10,27 @@
 <br>
 <table>
     <tr>
+
         <th>name</th>
         <th>surname</th>
         <th>department</th>
         <th>salary</th>
+        <th>Options</th>
 
     </tr>
     <c:forEach var="emp" items="${allEmps}">
+
+        <c:url var="updateButton" value="/updateInfo">
+            <c:param name="empId" value="${emp.id}"/>
+        </c:url>
         <tr>
             <td>${emp.name}</td>
             <td>${emp.surname}</td>
             <td>${emp.department}</td>
             <td>${emp.salary}</td>
+            <td>
+                <input type="button" value="Update" onclick="window.location.href ='${updateButton}"/>
+            </td>
         </tr>
 
     </c:forEach>
